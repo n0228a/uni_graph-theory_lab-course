aae13aea061a564e63902c58fd0146a3ac9e0372
n0228a <niklas@abele-netz.de>
2026-01-12 13:55:08 +0100

    included kernel

--- svm_dummy.py
+++ svm_dummy.py
@@ -9,10 +9,27 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 
 def my_kernel(X, Y):
     """
-    We create a custom kernel
+    Computes the intersection kernel between two arrays of sets.
+    K(x, y) = |x intersection y|
+    Args:
+        X: array-like of shape (n_samples_X,) containing python sets.
+        Y: array-like of shape (n_samples_Y,) containing python sets.
+
+    Returns:
+        K: Kernel matrix of shape (n_samples_X, n_samples_Y).
     """
 
-    return np.dot(X, Y)
+    n_x = len(X)
+    n_y = len(Y)
+
+    K = np.zeros((n_x, n_y))
+
+    for i in range(n_x):
+        for j in range(n_y):
+            # Compute intersection size
+            K[i, j] = len(X[i].intersection(Y[j]))
+
+    return K
 
 
 # we create an instance of SVM and fit out data.
@@ -20,5 +37,3 @@ clf = svm.SVC(kernel=my_kernel)
 
 clf.fit(X_train, Y_train)
 score = clf.score(X_test, Y_test)
-
-
